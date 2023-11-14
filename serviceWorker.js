193b8773fa937eb254fa70fb0093f3e873fae692
mrabdlrhman <mr.abdlrhman@gmail.com>
2023-11-14 10:51:38 +0300

    add pwa pocs

---
+++ serviceWorker.js
@@ -0,0 +1,27 @@
+const staticCourse = "course-site";
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/c1.jpg"
+
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticCourse).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
